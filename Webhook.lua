54c3c5f88d137d476800b14b9b4dd8f0b40731e4
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-05 23:52:48 +0100

    Create Webhook.lua

---
+++ Webhook.lua
@@ -0,0 +1,26 @@
+local Request = (syn and syn.request or request or http and http.request or http_request) or error("No request function found")
+local HttpService = game:GetService("HttpService")
+local Lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Robobo2022/notify-lib/main/lib'),true))()
+
+local Webhook = {}; do
+    function Webhook:Send(Webhook, Message)
+        local Succes, Error = pcall(function()
+            Request({
+                Url = Webhook,
+                Method = "POST",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                },
+                Body = HttpService:JSONEncode({
+                    ["content"] = Message
+                })
+            })
+        end)
+
+        if not Succes then
+            warn("Failed to send webhook: " .. Error)
+        end
+
+    end
+end
+return Webhook
