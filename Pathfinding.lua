0d1897111a3f8ba85b65bcf13d910c060ef40220
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-06 20:34:54 +0100

    Update Pathfinding.lua

--- Pathfinding.lua
+++ Pathfinding.lua
@@ -6,6 +6,8 @@ local Humanoid = lplr.Character:WaitForChild("Humanoid")
 local HumanoidRootPart = lplr.Character:WaitForChild("HumanoidRootPart")
 local PathFindingService = game:GetService("PathfindingService")
 local TweenSerivce = game:GetService("TweenService")
+local camera =  workspace.CurrentCamera
+local runservice = game:GetService("RunService")
 
 local pathfinding = {}
 
@@ -100,6 +102,23 @@ function pathfinding:TeleportTo(Position)
     end
 
     HumanoidRootPart.CFrame = CFrame.new(Waypoints[#Waypoints].Position)
+end
+
+function pathfinding:Path(Position)
+    local Begin
+    if Humanoid.RigType == Enum.HumanoidRigType.R15 then
+        Begin = Character.UpperTorso or Character.Torso;
+    elseif Character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
+        Begin = HumanoidRootPart;
+    end
+
+    local Path = PathFindingService:FindPathAsync(Begin.Position, Position)
+    local Waypoints = Path:GetWaypoints()
+
+    if #Waypoints == 0 then
+        Lib.prompt("Error", "No path found", 5)
+    end
+
 end
 
 return pathfinding
