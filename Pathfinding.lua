44791ba9e2cfa1935d9d021225cf6bbc43cbe79c
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-06 20:53:06 +0100

    Update Pathfinding.lua

--- Pathfinding.lua
+++ Pathfinding.lua
@@ -6,8 +6,6 @@ local Humanoid = lplr.Character:WaitForChild("Humanoid")
 local HumanoidRootPart = lplr.Character:WaitForChild("HumanoidRootPart")
 local PathFindingService = game:GetService("PathfindingService")
 local TweenSerivce = game:GetService("TweenService")
-local Camera =  workspace.CurrentCamera
-local Line = Drawing.new("Line")
 
 local pathfinding = {}
 
@@ -17,40 +15,45 @@ lplr.CharacterAdded:Connect(function()
 end)
 
 function pathfinding:MoveTo(Position, Wait)
-    local Begin
+    local Success, Error = pcall(function()
+        local Begin
 
-    if Humanoid.RigType == Enum.HumanoidRigType.R15 then
-        Begin = Character.UpperTorso or Character.Torso;
-    elseif Character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
-        Begin = HumanoidRootPart;
-    end
-
-    local Path = PathFindingService:FindPathAsync(Begin.Position, Position)
-    local Waypoints = Path:GetWaypoints()
-
-    if #Waypoints == 0 then
-        Lib.prompt("Error", "No path found", 5)
-    end
-
-    for Waypoint = 1, #Waypoints do
-        if Waypoints[Waypoint].Action == Enum.PathWaypointAction.Jump then
-            Humanoid.Jump = true
-            Humanoid:MoveTo(Waypoints[Waypoint + 1].Position)
-            Line.From = Vector2.new(Begin.Position)
-            Line.To = Vector2.new(Waypoints[Waypoint + 1].Position)
-
-            if Wait then
-                Humanoid.MoveToFinished:Wait()
-            end
-        else
-            Humanoid:MoveTo(Waypoints[Waypoint].Position)
-
-            if Wait then
-                Humanoid.MoveToFinished:Wait()
+        if Humanoid.RigType == Enum.HumanoidRigType.R15 then
+            Begin = Character.UpperTorso or Character.Torso;
+        elseif Character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
+            Begin = HumanoidRootPart;
+        end
+    
+        local Path = PathFindingService:FindPathAsync(Begin.Position, Position)
+        local Waypoints = Path:GetWaypoints()
+    
+        if #Waypoints == 0 then
+            Lib.prompt("Error", "No path found", 5)
+        end
+    
+        for Waypoint = 1, #Waypoints do
+            if Waypoints[Waypoint].Action == Enum.PathWaypointAction.Jump then
+                Humanoid.Jump = true
+                Humanoid:MoveTo(Waypoints[Waypoint + 1].Position)
+    
+                if Wait then
+                    Humanoid.MoveToFinished:Wait()
+                end
+            else
+                Humanoid:MoveTo(Waypoints[Waypoint].Position)
+    
+                if Wait then
+                    Humanoid.MoveToFinished:Wait()
+                end
             end
         end
-    end
-    Line:Destroy()
+
+        if not Success then
+            Lib.prompt("Error", "" .. Error, 5)
+        else
+            Lib.prompt("Success", "Moved to position", 5)
+        end
+    end)
 end
 
 function pathfinding:TweenTo(Position, Wait)
