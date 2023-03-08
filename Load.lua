433ae7d4e06663d513037f4beb736236798c00bb
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-08 07:22:56 +0100

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,5 +1,4 @@
 local utils = {
-    Webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/Webhook.lua"))(),
-    Pathfinding = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/Pathfinding.lua"))()
+    Webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/Webhook.lua"))()
 }
 return utils
