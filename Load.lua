a3dc6d450078c456b2a19573e844d7de4a9545e0
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-25 11:39:03 +0100

    oki

--- Load.lua
+++ Load.lua
@@ -1,4 +1,5 @@
 local utils = {
-    Webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/Webhook.lua"))()
+    Webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/Webhook.lua"))(),
+    ClosestPlayer = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/ClosestPlayer.lua"))(),
 }
 return utils
