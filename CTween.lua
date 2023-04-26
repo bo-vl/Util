2fc5103a7090462d59d996b6a566ddb87498a75a
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-26 20:24:26 +0200

    Update CTween.lua

--- CTween.lua
+++ CTween.lua
@@ -4,22 +4,26 @@ function ctween:go(player, endPosition, duration)
     local humanoidRootPart = player.Character.HumanoidRootPart
     local startPosition = humanoidRootPart.CFrame
     local startTime = os.clock()
+    local connection
 
     local function updatePosition()
         local elapsedTime = os.clock() - startTime
         if elapsedTime >= duration then
             humanoidRootPart.CFrame = endPosition
+            connection:Disconnect()
+            player.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
         else
             local t = elapsedTime / duration
             humanoidRootPart.CFrame = startPosition:Lerp(endPosition, t)
         end
     end
 
-    local connection = game:GetService("RunService").Heartbeat:Connect(updatePosition)
+    connection = game:GetService("RunService").Heartbeat:Connect(updatePosition)
 
     return function()
         connection:Disconnect()
     end
 end
+
 
 return ctween
