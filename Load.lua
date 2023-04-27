9a56db0dd1df33a7dc95846fa823c585759eb632
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-27 12:56:05 +0200

    ok

--- Load.lua
+++ Load.lua
@@ -1,6 +1,5 @@
 local utils = {
     Webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/Webhook.lua"))(),
     CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/CTween.lua"))(),
-    Pathfinding = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/Pathfinding.lua"))(),
 }
 return utils
