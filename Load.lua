5f73fcdcebc77641d386935c05dd658070f30934
bovanlaarhoven <bo.van.laarhoven@gmail.com>
2024-04-11 14:10:42 +0200

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,6 +1,5 @@
 local utils = {
     CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robobo2022/Util/main/CTween.lua"))(),
-    PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/Bovanlaarhoven/Util/main/Pathfinding.lua"))()
+    PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/Bovanlaarhoven/Util/main/Pathfinding.lua"))
 }
 return utils
-
